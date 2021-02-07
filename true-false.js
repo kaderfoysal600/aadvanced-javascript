440e51d168718dc8d0a57c8fa86abb4bd9050c70
kader foysal <kaderf113@gmail.com>
2021-02-07 00:57:34 -0800

    second commit

---
+++ true-false.js
@@ -0,0 +1,16 @@
+//falsy
+//false
+//0
+//""
+//undefined
+//null
+//truthy
+//' ',"0",[]
+
+let name = 'false';
+if(name|| name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
